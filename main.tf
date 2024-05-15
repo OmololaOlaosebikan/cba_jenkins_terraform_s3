4c65706e6372291bd637c3cbe407aec9fa04cfcf
OmololaOlaosebikan <110979954+OmololaOlaosebikan@users.noreply.github.com>
2024-05-15 03:43:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,20 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "s3test" {
+resource "aws_s3_bucket" "s3test-lola" {
     bucket = "myjenkinsbucket-lola"
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3test" {
-    bucket = aws_s3_bucket.s3test.id
+resource "aws_s3_bucket_ownership_controls" "s3test-lola" {
+    bucket = aws_s3_bucket.s3test-lola.id
     rule {
         object_ownership = "BucketOwnerPreferred"
     }
 }
 
-resource "aws_s3_bucket_acl" "s3test" {
-    depends_on = [aws_s3_bucket_ownership_controls.s3test]
+resource "aws_s3_bucket_acl" "s3test-lola" {
+    depends_on = [aws_s3_bucket_ownership_controls.s3test-lola]
 
-    bucket = aws_s3_bucket.s3test.id
+    bucket = aws_s3_bucket.s3test-lola.id
     acl    = "private"
 }
